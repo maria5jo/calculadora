0082ac24903256b577d6d3788cc2cf84d599d652
maria5jo <mariase3105@gmail.com>
2022-09-08 10:36:50 -0500

    primera version

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Calculadora from './Components/Calculadora'
+const App = () => {
+  return (
+    <Calculadora/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  )
+}
+
+export default App
